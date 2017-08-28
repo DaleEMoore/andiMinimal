dddfb2e1bfa5e89adaf782670d89e1c1715bd82c
Dale E. Moore <DaleEMoore@gMail.Com>
2017-08-28 16:17:15 -0500

    in sync

---
+++ main.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3
+
+__version__ = "0.1" # TODO; increment this frequently!
+
+# cd ~/PycharmProjects/andiMinimal
+# buildozer init
+# vi buildozer.spec
+# buildozer android debug
+# email bin/*.apk to phone
+# Install apk on phone.
+
+print("Start andiMinimal version " + str(__version__))
+print("End andiMinimal version " + str(__version__))

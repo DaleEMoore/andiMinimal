56314b4673319125b386122ab3abbefd746cfd37
Dale E. Moore <DaleEMoore@gMail.Com>
2017-08-28 18:14:42 -0500

    in sync

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python3
 
-__version__ = "0.1" # TODO; increment this frequently!
+__version__ = "0.2" # TODO; increment this frequently!
 
 # cd ~/PycharmProjects/andiMinimal
 # buildozer init
@@ -8,6 +8,16 @@ __version__ = "0.1" # TODO; increment this frequently!
 # buildozer android debug
 # email bin/*.apk to phone
 # Install apk on phone.
+
+# But, I can't see the prints anywhere.
+# https://stackoverflow.com/questions/23055696/see-output-of-print-statements-on-android-using-kivy-kivy-launcher
+# https://stackoverflow.com/questions/3102443/is-there-a-way-to-print-to-the-console-in-an-android-app
+# Nice printlog():
+# https://stackoverflow.com/questions/26265015/how-to-get-console-output-printed-using-kivy
+# https://developer.android.com/reference/android/util/Log.html
+
+# go look at andiKivyPrint
+
 
 print("Start andiMinimal version " + str(__version__))
 print("End andiMinimal version " + str(__version__))
